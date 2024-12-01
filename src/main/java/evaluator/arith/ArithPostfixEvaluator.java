2c2bd92f83fadd61d796910ef64a7518f8483cf5
aiden <s25aszydlik@severnschool.com>
2024-12-01 17:54:07 -0500

    I do not understand how my divide by 0 does not work
    Will work on invalid expression tests

--- src/main/java/evaluator/arith/ArithPostfixEvaluator.java
+++ src/main/java/evaluator/arith/ArithPostfixEvaluator.java
@@ -3,6 +3,7 @@ package evaluator.arith;
 import evaluator.Evaluator;
 import language.Operand;
 import language.Operator;
+import language.arith.UnaryOperator;
 import parser.arith.ArithPostfixParser;
 import stack.LinkedStack;
 import stack.StackInterface;
@@ -37,14 +38,26 @@ public class ArithPostfixEvaluator implements Evaluator<Integer> {
                     stack.push(operand);
                     break;
                 case OPERATOR:
-                    Operator<Integer> operator = parser.nextOperator();
-                    Operand<Integer> operand2 = stack.pop();
-                    Operand<Integer> operand1 = stack.pop();
-                    operator.setOperand(0, operand1);
-                    operator.setOperand(1, operand2);
 
-                    Operand<Integer> result = operator.performOperation();
-                    stack.push(result);
+                    Operator<Integer> operator = parser.nextOperator();
+
+                    if (operator instanceof UnaryOperator) {
+                        // Unary operator (e.g., negation)
+                        Operand<Integer> operandToNegate = stack.pop();
+                        operator.setOperand(0, operandToNegate);
+                        Operand<Integer> result = operator.performOperation();
+                        stack.push(result);
+                    }
+                    else {
+
+                        Operand<Integer> operand2 = stack.pop();
+                        Operand<Integer> operand1 = stack.pop();
+                        operator.setOperand(0, operand1);
+                        operator.setOperand(1, operand2);
+
+                        Operand<Integer> result = operator.performOperation();
+                        stack.push(result);
+                    }
                     break;
                 default:
                     throw new IllegalStateException("Unexpected token type");
